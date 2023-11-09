440a78755f2b5e63fe7083c872532dbb795a8ae8
Andrea N <andrea@narbot.com>
2023-11-09 18:12:48 -0500

    Delete Netlify toml

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,8 +16,26 @@ module.exports = {
         },
         kontentai: '#3dcca8',
         elitebuild: '#0078d2',
+        primary: {
+          light: '#F3E5F5',
+          DEFAULT: '#7E57C2',
+          dark: '#4527A0',
+        },
+        secondary: {
+          light: '#E1F5FE',
+          DEFAULT: '#03A9F4',
+          dark: '#0288D1',
+        },
+        tertiary: {
+          light: '#FFFDE7',
+          DEFAULT: '#FFC107',
+          dark: '#FFA000',
+        },
       },
     },
   },
   plugins: [require('@tailwindcss/typography')],
 };
+ .my-element {
+  background-color: theme('colors.manufacturing.DEFAULT');
+}
