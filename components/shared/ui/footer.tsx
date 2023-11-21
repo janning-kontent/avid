8332252bcd6494f59f1eabf1b3a157d18c669df2
Andrea N <andrea@narbot.com>
2023-11-20 20:25:48 -0500

    Corrected Name

--- components/shared/ui/footer.tsx
+++ components/shared/ui/footer.tsx
@@ -130,7 +130,7 @@ export const Footer: FC<Props> = (props) => {
       <div className='flex items-center mx-auto max-w-screen-xl px-4'>
         <div className='w-screen h-full md:flex justify-between z-40 md:pr-24 xl:pr-12 2xl:pr-0'>
           <div className='flex flex-row w-full justify-center'>
-            <div>AVID Education</div>
+            <div>AVID Center</div>
             <div className='border-l-2 pl-4 ml-4'>Copyright {currentYear}</div>
 
             {/* <MenuList
